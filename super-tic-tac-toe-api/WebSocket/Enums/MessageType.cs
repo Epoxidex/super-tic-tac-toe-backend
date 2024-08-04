ff22a9bc6f0d92a44ae5788528ea604e63652a6f
Epoxidex <skosarevivan@yandex.ru>
2024-08-04 21:18:14 +0300

    add MessageType

---
+++ super-tic-tac-toe-api/WebSocket/Enums/MessageType.cs
@@ -0,0 +1,16 @@
+﻿namespace super_tic_tac_toe_api.WebSocket.Enums
+{
+    public enum MessageType
+    {
+        Joined = 101,
+        Move = 102,
+
+        IvalidParams = 200,
+        LobbyNotFound = 201,
+        PlayerAlreadyExist = 202,
+        LobbyIsFull = 203,
+        PlayerNotFound = 204,
+        NotYourTurn = 205,
+        BadMove = 206,
+    }
+}
